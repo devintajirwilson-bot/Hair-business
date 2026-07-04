375ec837546637d5a7e54f0f1f9209cca28efb08
devinWilsonChi1 <devint.wilson@hgs.hiddengeniusproject.org>
2026-07-03 22:29:11 -0500

    first upload

--- script.js
+++ script.js
@@ -1,8 +1,42 @@
-document.getElementById("bookingForm").addEventListener("submit", function(e) {
-  e.preventDefault();
+const words = {
 
-  document.getElementById("msg").innerText =
-    "Your booking has been received. We will contact you soon.";
+en:{
 
-  this.reset();
-});
+hero:"Luxury Hair Starts Here",
+
+subtitle:"Luxury Braids • Locs • Silk Press • Color",
+
+book:"Book Appointment"
+
+},
+
+fr:{
+
+hero:"Le Luxe Commence Ici",
+
+subtitle:"Tresses • Locks • Lissage • Coloration",
+
+book:"Prendre Rendez-vous"
+
+}
+
+};
+
+function changeLanguage(language){
+
+document.querySelector(".hero h1").innerHTML=
+words[language].hero;
+
+document.querySelector(".hero p").innerHTML=
+words[language].subtitle;
+
+document.querySelector(".hero a").innerHTML=
+words[language].book;
+
+}
+
+document.querySelectorAll(".language button")[0]
+.onclick=()=>changeLanguage("en");
+
+document.querySelectorAll(".language button")[1]
+.onclick=()=>changeLanguage("fr");
